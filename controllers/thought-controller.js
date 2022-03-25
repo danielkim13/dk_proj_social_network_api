8d6c485305e93077ef9a942af0b16021ff8eddd5
Daniel Kim <daniel.d.kim13@gmail.com>
2022-03-25 02:41:23 -0400

    had some hiccup w/ thought creation

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -15,15 +15,17 @@ const thoughtController = {
     }
   },
 
-  //   create a thought
-  async createThought({ params, body }, res) {
+  //   create a thought need to ask how to create async with this case.
+  async createThought({ body }, res) {
     try {
       const dbThought = await Thought.create(body).then(({ _id }) => {
-        return User.findOneAndUpdate({ _id: params.userId }, { $push: { thoughts: _id } }, { new: true });
+        console.log(_id);
+        return User.findOneAndUpdate({ _id: body.userId }, { $push: { thoughts: _id } }, { new: true });
       });
       if (!dbThought) {
         res.status(404).json({ message: "No user found with this id!" });
       }
+
       res.json(dbThought);
     } catch (err) {
       res.status(500).json(err);
